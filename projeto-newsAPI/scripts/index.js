6c933994ac58af0ecb19cd34f4cd59db550e0e95
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-03-09 18:38:38 -0300

    Update UX Menu | Feat :hamburger:

--- projeto-newsAPI/scripts/index.js
+++ projeto-newsAPI/scripts/index.js
@@ -1,3 +1,135 @@
 import { chamarApi } from "./usserCases.js";
 
-chamarApi(2, 1)
+chamarApi(input, result, search)
+
+
+
+
+
+// controle do menu
+var iconHambug = document.getElementById('iconHamburguer')
+var iconX = document.getElementById('iconX')
+var menuSide = document.getElementById('menuSide')
+
+
+iconX.addEventListener('click', ocultar)
+iconHambug.addEventListener('click', clicou)
+
+function clicou() {
+
+    menuSide.classList.remove('desaparecer')
+    menuSide.classList.add('surgir')
+    menuSide.classList.remove('init')
+
+    iconHambug.style.opacity = '0'
+    iconX.classList.add('iconX-surgir')
+    iconX.classList.remove('init')
+
+
+}
+
+function ocultar(){
+
+    menuSide.classList.add('desaparecer')
+    
+    setTimeout(function(){
+        menuSide.classList.add('init')
+
+    },300)
+
+    iconX.classList.add('init')
+    iconHambug.style.opacity = '1'
+    iconX.classList.remove('iconX-surgir')
+    menuSide.classList.remove('surgir')
+
+
+}
+
+menuSide.addEventListener('click', function(event){
+    const clicouDentro = menuSide.contains(event.target)
+
+    if(!clicouDentro){
+        ocultar()
+    }
+
+})
+
+
+
+var ulForSearch = document.getElementById('forSearch')
+var ulForResult = document.getElementById('forResult')
+var ulForCredits = document.getElementById('credits')
+
+ulForCredits.addEventListener('click', mostrarCredits)
+ulForResult.addEventListener('click', mostrarResults)
+ulForSearch.addEventListener('click', mostrarSearch)
+
+
+function mostrarResults(){
+    displaySubtItensResult()
+    changeUl()
+}
+
+function mostrarSearch(){
+    displaySubItensSearch()
+    changeUl()
+}
+
+function mostrarCredits(){
+    displaySubItensCredits()
+    changeUl()
+}
+
+function displaySubtItensResult(){
+
+    ulForResult.classList.add('exibirSub')
+
+}
+
+function displaySubItensSearch(){
+
+    ulForSearch.classList.add('exibirSub')
+
+}
+
+function displaySubItensCredits(){
+
+    ulForCredits.classList.add('exibirSub')
+
+}
+
+let ul = ulForCredits
+
+function changeUl(){
+
+    if(ulForCredits.classList.contains('exibirSub')){
+
+        ul = ulForCredits
+
+    } else if(ulForResult.classList.contains('exibirSub')){
+
+        ul = ulForResult
+
+    } else if(ulForSearch.classList.contains('exibirSub')){
+
+        ul = ulForSearch
+
+    } 
+}
+
+changeUl()
+
+
+document.addEventListener('click', function(evento){
+    const clicouDentro = menuSide.contains(evento.target)
+    const clicadoDentroDoElemento = ul.contains(evento.target)
+
+    if(!clicadoDentroDoElemento){
+        ul.classList.remove('exibirSub')
+    }
+
+    changeUl()
+
+
+
+})
