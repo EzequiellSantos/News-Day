83b4f7d13e2487e33d0038641f2e3bff5da788e0
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-03-09 18:39:07 -0300

    Update Basic Structure | Test :new:

--- projeto-newsAPI/scripts/usserCases.js
+++ projeto-newsAPI/scripts/usserCases.js
@@ -1,19 +1,40 @@
-export function chamarApi(busca, resposta) {
-
+export async function chamarApi(busca, resposta) {
+    
     try{
 
-        if(true){ // verificações para saber qual criterio de busca e resposta o usuário escolheu
+      const response = await fetch(apiCountryURL)
 
-            console.log(busca + resposta)
+      if(response.ok){
 
-        }
+        const dados = await response.json()
+        console.log(dados)
 
-        
+      } else{
 
-    } catch (erro){
+        alert('Erro: ', response.status)
 
-        alert("Erro: ", erro.message)
+      }
+
+
+    } catch (error){
+
+      alert('Erro: ', error.mensage)
 
     }
 
 }
+
+var date = new Date()
+var day = date.getDate()
+var mes = date.getMonth()
+var mesAtual = mes + 1
+
+const keyAPI = '8164a1687d9e4d80a5901e71edaf039c'
+
+// para noticias atuais de um país ou categoria
+var country = 'br'
+var categoria = 'technology'
+var q = 'lula' //palavras chaves ou uma frase para pesquisar
+const apiCountryURL = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${keyAPI}`
+/*     const apiCategoriaURL = `https://newsapi.org/v2/top-headlines?sources=${categoria}&apiKey=${keyAPI}`
+const apiPalavraURL = `https://newsapi.org/v2/top-headlines?q=${q}&apiKey=${keyAPI}` */
