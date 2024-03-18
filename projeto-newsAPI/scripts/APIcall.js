411f8ed02a77adc100adedfe54b3eadec32a2c37
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-03-18 12:34:33 -0300

    Update CallAPi | Refacto :up:

--- projeto-newsAPI/scripts/APIcall.js
+++ projeto-newsAPI/scripts/APIcall.js
@@ -1,3 +1,179 @@
-import {} from ""
+import { exbirErroStatus, exibirFrontEnd, tooManyCalls } from "./frontEnd.js"
+import { artigos, limparArtigos } from "./backEnd.js"
+import { limparUserSearch, pesquisaUser } from "./index.js"
 
 //script de todo o código de consumo da API (trasnformações de url, seleção dos dados e status da chamadas)
+
+var date = new Date()
+var day = date.getDate()
+var mes = date.getMonth()
+var ano = date.getFullYear()
+var mesAtual = mes + 1
+let dateToday = `${ano}-${mesAtual.toString().padStart(2, '0')}-${day - 1}`
+
+const keyAPI = '8164a1687d9e4d80a5901e71edaf039c'
+
+// chamadas de  api
+
+var url = ''
+
+export function definirUrls(search, result) {
+
+    if (search == 'Palavra-Chave') {
+
+        adequedUrlKeyWord(pesquisaUser, result)
+
+    } else if (search == 'Domínio') {
+
+        adequarUrlDomain(pesquisaUser, result)
+
+    }
+
+
+    limparUserSearch()
+    chamarAPI(url)
+
+}
+
+function adequedUrlKeyWord(userSearch, result) {
+
+    if (result == 'Data') {
+
+        url = `https://newsapi.org/v2/everything?q=${userSearch}&from=${dateToday}&to=${dateToday}&sortBy=publishedAt&apiKey=${keyAPI}`
+
+    } else if (result == 'Popularidade') {
+
+        url = `https://newsapi.org/v2/everything?q=${userSearch}&sortBy=popularity&apiKey=${keyAPI}`
+
+    } else if (result == 'Relevância') {
+
+        url = `https://newsapi.org/v2/everything?q=${userSearch}&sortBy=relevancy&apiKey=${keyAPI}`
+
+    } else if (result == 'Última-Hora') {
+
+        url = `https://newsapi.org/v2/top-headlines?q=${userSearch}&apiKey=${keyAPI}`
+
+    }
+
+    console.log(userSearch)
+
+}
+
+function adequarUrlDomain(userSearch, result) {
+
+    if (result == "Data") {
+
+        url = `https://newsapi.org/v2/everything?domains=${userSearch}&from=${dateToday}&to=${dateToday}&sortBy=publishedAt&apiKey=${keyAPI}`
+
+    } else if (result == 'Popularidade') {
+
+        url = `https://newsapi.org/v2/everything?domains=${userSearch}&sortBy=popularity&apiKey=${keyAPI}`
+
+    } else if (result == 'Relevância') {
+
+        url = `https://newsapi.org/v2/everything?domains=${userSearch}&sortBy=relevancy&apiKey=${keyAPI}`
+
+    } else if (result == 'Última-Hora') {
+
+        url = `https://newsapi.org/v2/top-headlines?domains=${userSearch}&apiKey=${keyAPI}`
+
+    }
+
+    console.log(userSearch)
+
+}
+
+export let sucessResult = null
+export let callsSussced = 0
+
+export async function chamarAPI(apiURL) {
+
+    try {
+
+        var response = await fetch(apiURL)
+
+        if (response.ok) {
+
+            var dados = await response.json()
+            consumirDados(dados)
+            console.log(dados)
+            console.log(pesquisaUser)
+
+            sucessResult = true
+            callsSussced += 1
+
+        } else {
+
+            sucessResult = false
+            
+
+            if (response.status == 400) {
+
+                console.error('Não encontrado esse tipo de pesquisa')
+                exbirErroStatus()
+
+            } else if (response.status == 429) {
+
+                tooManyCalls()
+                console.error('Você Fez Muitas solicitações')
+
+            } else if (response.status == 500) {
+
+                console.error('Nosso Provedor de Noticias Está com problemas')
+
+            } else {
+
+                console.log(response.status)
+
+            }
+
+        }
+
+
+    } catch (error) {
+
+        console.log('Erro: ', error.message)
+        
+
+    }
+
+}
+
+export let quantidArtigos = 0
+
+async function consumirDados(dados) {
+
+    quantidArtigos = await dados.articles.length >= 1 ? 1 : dados.articles.length
+
+    if(quantidArtigos == 0){
+        limparArtigos()
+        exbirErroStatus()
+    } else{
+        
+        for (let t = 0; t <= quantidArtigos; t++) {
+
+
+            artigos.manchetes.autor.push(dados.articles[t].author)
+    
+            artigos.manchetes.url.push(dados.articles[t].url)
+    
+            artigos.manchetes.urlImage.push(dados.articles[t].urlToImage)
+    
+            artigos.manchetes.font.push(dados.articles[t].source.name)
+    
+            artigos.manchetes.titulo.push(dados.articles[t].title)
+    
+            artigos.manchetes.descricao.push(dados.articles[t].description)
+    
+            artigos.manchetes.texto.push(dados.articles[t].content)
+    
+            artigos.manchetes.data.push(dados.articles[t].publishedAt)
+    
+        }
+
+        exibirFrontEnd()
+
+    }
+
+
+}
--- projeto-newsAPI/scripts/backEnd.js
+++ projeto-newsAPI/scripts/backEnd.js
@@ -1,3 +1,205 @@
-import {} from ""
+import { limparUserSearch, pesquisaUser } from "./index.js"
+import {} from "./APIcall.js"
+import { antigoSearch, articleSub, exibirCartaoEntrada, tittleContainer, updatePlaceHolder } from "./frontEnd.js"
 
 // sript da junção dos dados da api e preparação das informações para exibir na api
+
+
+export let artigos = {
+
+    manchetes: {
+  
+      url: ['jd9ehd'],
+      urlImage: ['ndevheh'],
+      font: [],
+      data: [],
+      autor: [],
+      titulo: [],
+      descricao: [],
+      texto: [ 'dhchwudhcwbdc']
+  
+    }
+  
+  
+}
+
+// adequando o back para front
+
+export let busca = ''
+export let resultado = ''
+
+export function adequedFrontEnd(search, results){
+
+
+
+  switch(search){
+    case 'checkPalavraChave':
+      busca = 'Palavra-Chave'
+      break
+
+    case 'checkDominio':
+      busca = 'Domínio'
+      break
+  }
+
+  switch(results){
+    case 'checkDate':
+      resultado = 'Data'
+      break
+    
+    case 'checkRelevance':
+      resultado = 'Relevância'
+      break
+    
+    case 'checkPopularity':
+      resultado = 'Popularidade'
+      break
+
+    case 'checkTop':
+      resultado = 'Última-Hora'
+      break
+  }
+
+  
+  updatePlaceHolder(busca)
+
+}
+
+// coletar os critérios de busca do usuário
+export var inputsChecks = []
+export function coletingChoices(){
+
+    inputsChecks = []
+    const inputsRadio = document.querySelectorAll('.box')
+
+    inputsRadio.forEach(input => {
+
+        if(input.checked){
+
+            inputsChecks.push(input.id)
+            
+        }
+
+    })
+
+    adequedFrontEnd(inputsChecks[0], inputsChecks[1])
+    updatePlaceHolder(busca)
+    
+
+}
+
+// armazenando os dados de pesquisas do usuário
+export function updateLocalStorage(){
+
+    localStorage.setItem('lastSearch', pesquisaUser)
+    localStorage.setItem('LastChoiceSearch', inputsChecks[0])
+    localStorage.setItem('LastChoiceResult', inputsChecks[1])
+
+}
+
+// verificando se é a primeira visita do usuário na página
+
+export let lastSearch = localStorage.getItem('lastSearch')
+export let LastChoiceSearch = localStorage.getItem('LastChoiceSearch')
+export let LastChoiceResult =  localStorage.getItem('LastChoiceResult') 
+export let permissionUser = false
+
+export function firstVisitUser(){
+  
+  if( lastSearch && LastChoiceSearch && LastChoiceResult && permissionUser){
+
+    /* ativar os inputs presentes no Busca e Resultado, e fazer uma chamada a API */
+    console.log('localStorage => ', lastSearch, busca, resultado)
+    adequedFrontEnd(LastChoiceSearch, LastChoiceResult)
+
+  } else{
+    /* adicionar uma imagem de sem pesquisas no article SubContainer */
+    exibirCartaoEntrada()
+    console.log('Você não tem pesquisas recentes guardadas')
+    cleanLocalStorage()
+  }
+
+}
+
+// limpar o local storage
+export function cleanLocalStorage(){
+
+    localStorage.removeItem('lastSearch')
+    localStorage.removeItem('LastChoiceSearch')
+    localStorage.removeItem('LastChoiceResult')
+
+
+}
+
+
+//limpar os artigos e titulos
+export function limparArtigos(){
+
+    for(let tai in artigos.manchetes){ // limpando os artigos armazenados da ultima pesquisa
+        
+        if(artigos.manchetes[tai].length != 0){
+
+            artigos.manchetes[tai] = []
+
+        }
+
+    }
+
+    var containers = document.querySelectorAll('.container')
+
+    if(articleSub.hasChildNodes() || tittleContainer.hasChildNodes()){
+
+        containers.forEach( function(container) {
+
+            var filhos = container.childNodes
+    
+            filhos.forEach(function(filho){
+                filho.remove()
+            })
+
+            limparChildren()
+    
+    
+        })
+
+
+        
+    } else{
+
+        console.log('parece que nao tem filhos')
+
+    }
+
+
+
+
+    return console.log('limpando')  
+
+  
+}
+
+export function limparChildren(){
+    var childrens = document.querySelectorAll('.chidren')
+
+    childrens.forEach(function(children){
+        children.remove()
+    })
+}
+
+export function checklogSearchs(){
+    if(antigoSearch != pesquisaUser){
+        limparArtigos()
+        limparChildren()
+
+        return pesquisaUser
+
+    }else{
+        return
+    }
+}
+
+// testes
+export function test(item0, item1){
+    console.log(' - >Teste ', item0, item1)
+    console.log(' - >Typeof ', typeof(item0), typeof(item1))
+}
